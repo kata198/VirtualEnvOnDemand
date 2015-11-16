ace7cb8e8938c4b5530a2193e0b28c639990ff08
Tim Savannah <kata198@gmail.com>
2015-11-16 14:28:19 -0500

    Initial Checkin, working but needs documentation

---
+++ VirtualEnvOnDemand/exceptions.py
@@ -0,0 +1,20 @@
+# vim: ts=4 sw=4 expandtab
+
+class PipInstallFailed(Exception):
+    '''
+        PipInstallFailed - Exception raised when pip fails to install a list of packages
+    '''
+
+    def __init__(self, returnCode=None, reqFileContents=''):
+        '''
+            Create a PipInstallFailed exception, building a message from some pieces of information
+
+                @param returnCode <int> - Return code of subprocess, or None if undetermined
+                @param reqFileContents <str> - String of requirements file to reproduce error
+        '''
+        self.returnCode = returnCode
+        self.reqFileContents = reqFileContents
+        Exception.__init__(self, self._genMsg())
+
+    def _genMsg(self):
+        return 'Failed to install pip modules (ret=%s) using this requirements file: \n"""\n%s\n"""\n\nCheck stdout/stderr logs.' %(str(self.returnCode), str(self.reqFileContents))
---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2015 Timothy Savannah under terms of LGPLv3. You should have received a copy of this with this distribution as "LICENSE"
+
+
+#vim: set ts=4 sw=4 expandtab
+
+import os
+from setuptools import setup
+
+
+if __name__ == '__main__':
+
+    dirName = os.path.dirname(__file__)
+    if dirName and os.getcwd() != dirName:
+        os.chdir(dirName)
+
+    summary = 'Module which supports installing and using dependencies within a live python application'
+
+    try:
+        with open('README.rst', 'r') as f:
+            long_description = f.read()
+    except Exception as e:
+        sys.stderr.write('Exception when reading long description: %s\n' %(str(e),))
+        long_description = summary
+
+    setup(name='VirtualEnvOnDemand',
+            version='1.0.0',
+            packages=['VirtualEnvOnDemand'],
+            author='Tim Savannah',
+            author_email='kata198@gmail.com',
+            maintainer='Tim Savannah',
+            url='https://github.com/kata198/VirtualEnvOnDemand',
+            maintainer_email='kata198@gmail.com',
+            requires=['virtualenv'],
+            install_requires=['virtualenv'],
+            description=summary,
+            long_description=long_description,
+            license='LGPLv3',
+            keywords=['virtualenv', 'on', 'demand', 'pip', 'install', 'import', 'runtime', 'ImportError', 'reload', 'module'],
+            classifiers=['Development Status :: 4 - Beta',
+                         'Programming Language :: Python',
+                         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+                         'Programming Language :: Python :: 2',
+                          'Programming Language :: Python :: 2',
+                          'Programming Language :: Python :: 2.6',
+                          'Programming Language :: Python :: 2.7',
+                          'Programming Language :: Python :: 3',
+                          'Programming Language :: Python :: 3.3',
+                          'Programming Language :: Python :: 3.4',
+                          'Topic :: System :: Installation/Setup',
+                          'Topic :: Software Development :: Libraries :: Python Modules',
+            ]
+    )
+
