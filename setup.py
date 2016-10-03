e69535b232f63586838eded8f5a5888f54dabe3e
Tim Savannah <kata198@gmail.com>
2016-10-03 18:54:43 -0400

    5.0.3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         long_description = summary
 
     setup(name='VirtualEnvOnDemand',
-            version='5.0.2',
+            version='5.0.3',
             packages=['VirtualEnvOnDemand'],
             author='Tim Savannah',
             author_email='kata198@gmail.com',
