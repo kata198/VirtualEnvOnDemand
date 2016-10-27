21d0a75c952e060adbaf6bc0a971eea3af2cabd1
Tim Savannah <kata198@gmail.com>
2016-10-27 18:54:38 -0400

    Update docs

--- mkdoc.sh
+++ mkdoc.sh
@@ -4,3 +4,4 @@ pydoc -w `echo VirtualEnvOnDemand/*.py | sed 's/.py//g' | sed 's|/__init__||g' |
 mv *.html doc/
 echo "Running fixdoc.py"
 ./fixdoc.py
+cp -f doc/VirtualEnvOnDemand.html doc/index.html
