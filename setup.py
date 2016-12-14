17700f7f6612528a17c53cf2f897bae19b9e6753
Tim Savannah <kata198@gmail.com>
2016-12-14 13:42:14 -0500

    Changelog and tag 6.0.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         long_description = summary
 
     setup(name='VirtualEnvOnDemand',
-            version='5.1.1',
+            version='6.0.0',
             packages=['VirtualEnvOnDemand'],
             author='Tim Savannah',
             author_email='kata198@gmail.com',
